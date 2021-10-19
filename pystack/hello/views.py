a442e0857ce3cc9329537624d90a1a5735bf19e4
miguelribeirooo <rrmiguel018@gmail.com>
2021-10-19 16:54:41 -0300

    project_django

---
+++ pystack/hello/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Batatinha frita 1 2 3")
--- pystack/usuarios/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Usuario
-
-@admin.register(Usuario)
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'email', 'senha')
-    search_fields = ('nome', 'email')
-    readonly_fields = ('senha',)
